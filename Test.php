2f8c0af533d93719a71a34fda83f20da38fdb733
Witchplume <94255134+Witchplume@users.noreply.github.com>
2021-11-17 23:01:40 +0100

    Removed API Key

--- Test.php
+++ Test.php
@@ -8,17 +8,17 @@ use RiotAPI\Base\Definitions\Region;
 //  Initialize the library
 $api = new LeagueAPI([
 	//  Your API key, you can get one at https://developer.riotgames.com/
-	LeagueAPI::SET_KEY    => 'RGAPI-f7045a96-4274-4f2a-a041-39303cf4eaa5',
+	LeagueAPI::SET_KEY    => 'API Key here',
 	//  Target region (you can change it during lifetime of the library instance)
 	LeagueAPI::SET_REGION => Region::EUROPE_WEST,
 ]);
 
 //  And now you are ready to rock!
-$summoner = $api->getSummonerByName("StorSnefnug");
+$summoner = $api->getSummonerByName("Witchplume");
 $matchlist = $api->getMatchIdsByPUUID($summoner->puuid);
 
 print_r($summoner) . '<br>' . '<br>';
 print_r($matchlist);
 
-echo "<img src=\"$summoner->profileIconId.png\">";
+echo "<img src=\"img\\profileicon\\$summoner->profileIconId.png\">";
 ?>
