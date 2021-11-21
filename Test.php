ec067965499d1266768856b17d00374e87c9111a
Witchplume <94255134+Witchplume@users.noreply.github.com>
2021-11-21 17:51:17 +0100

    Update Test.php
    Extra testing, trying to get GetMatch to work

--- Test.php
+++ Test.php
@@ -1,24 +1,50 @@
 <?php  
-	//  Include all required files (installation via Composer is required)
-require_once __DIR__  . "/vendor/autoload.php";
+		//  Include all required files (installation via Composer is required)
+	require_once __DIR__  . "/vendor/autoload.php";
 
-use RiotAPI\LeagueAPI\LeagueAPI;
-use RiotAPI\Base\Definitions\Region;
+	use RiotAPI\LeagueAPI\LeagueAPI;
+	use RiotAPI\Base\Definitions\Region;
 
-//  Initialize the library
-$api = new LeagueAPI([
-	//  Your API key, you can get one at https://developer.riotgames.com/
-	LeagueAPI::SET_KEY    => 'API Key here',
-	//  Target region (you can change it during lifetime of the library instance)
-	LeagueAPI::SET_REGION => Region::EUROPE_WEST,
-]);
+	//  Initialize the library
+	$api = new LeagueAPI([
+		//  Your API key, you can get one at https://developer.riotgames.com/
+		LeagueAPI::SET_KEY    => 'APIKEY',
+		//  Target region (you can change it during lifetime of the library instance)
+		LeagueAPI::SET_REGION => Region::EUROPE_WEST,
+	]);
 
-//  And now you are ready to rock!
-$summoner = $api->getSummonerByName("Witchplume");
-$matchlist = $api->getMatchIdsByPUUID($summoner->puuid);
+	//  And now you are ready to rock!
+	$summoner = $api->getSummonerByName("Witchplume");
+	$matchlist = $api->getMatchIdsByPUUID($summoner->puuid);
 
-print_r($summoner) . '<br>' . '<br>';
-print_r($matchlist);
+	//print_r($summoner) . '\n';
 
-echo "<img src=\"img\\profileicon\\$summoner->profileIconId.png\">";
+	foreach ($summoner->getData() as $summoner_info) {
+		echo $summoner_info . '<br>';
+	}
+
+	echo '<br>';
+
+	foreach ($matchlist as $match) {
+		echo $match . '<br>';
+	}
+
+	$summonerpuuid = $api->getSummonerByPUUID("YRrea91Rb8DKyHvocGIwl-Fg7DDmiSPHTn2sXA_nrfiQTFo27IT9gwrWPe_IY70wODcqc7NemIr9ug");
+
+	foreach ($summonerpuuid as $MatchSummoner1) {
+		echo '<br>' . $MatchSummoner1;
+	}
+
+	/*
+	$matchcurrent = $api->GetMatch("EUW1_5566980115");
+
+	foreach ($matchcurrent as $Match1) {
+		echo '<br>' . $Match1;
+	}
+	*/
+
+
+	//echo $api->getMatch('EUW1_5566980115');
+
+	//echo "<img src=\"img\\profileicon\\$summoner->profileIconId.png\">"; Works
 ?>
